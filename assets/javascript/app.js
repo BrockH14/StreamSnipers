53a25555ce4cd68f046483a413be585358416c3d
Brett Landes <brettl0407@gmail.com>
2020-03-21 14:38:53 -0400

    nested ajax calls to use the game id variable in stream finder ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,65 @@
-
-// $("#submit").on("click", function(){
-//     event.preventDefault();
-    
-//     $.ajax({
-//         type: 'GET',
-//         url: 'https://api.twitch.tv/helix/games/top',
-//         headers: {
-//           'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2'
-//         },
-//         success: function(data) {
-//           console.log(data);
-//         }
-//        });
-
-// })
 var gameID;
 
-
-$.ajax({
-    type: 'GET',
-    url: 'https://api.twitch.tv/helix/games/top',
-    headers: {
-      'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2',
-      'Accept': 'application/vnd.twitchtv.v5+json'
-    },
-    success: function(data) {
-      console.log(data);
-    }
-   });
-
-
- console.log("----------------------------------------------------------------");
-   //Build url with search option-------------------------------------------------
-
-  $.ajax({
-     type: 'GET',
-     url: 'https://api.twitch.tv/kraken/search/games?query=call%20of%20duty',
-     headers: {
-       'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2',
-       'Accept': 'application/vnd.twitchtv.v5+json'
-     },
-     success: function(data) {
-       console.log(data);
-       var gameID = data.games[0]._id;
-       console.log(gameID);
-     }
-});
+function streamAjax() {
+    
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/helix/streams?game_id=' + gameID + '',
+        headers: {
+          'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2'
+        },
+        success: function(data) {
+          console.log(data);
+        }
+       });
+}
 
 
+
+
+
+
+
+
+//  $("#submit").on("click", function(){
+//      event.preventDefault();
+    
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.twitch.tv/helix/games/top',
+        headers: {
+          'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2',
+          'Accept': 'application/vnd.twitchtv.v5+json'
+        },
+        success: function(data) {
+          console.log(data);
+        }
+       });
    
-  //build url with search option-----------------------------------------------------
-
    
- $.ajax({
-    type: 'GET',
-    url: 'https://api.twitch.tv/helix/streams/game_id=' + gameID + 'limit=5',
-    headers: {
-      'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2'
-    },
-    success: function(data) {
-      console.log(data);
-    }
-   });
+       
+       //Build url with search option-------------------------------------------------
+   
+      $.ajax({
+         type: 'GET',
+         url: 'https://api.twitch.tv/kraken/search/games?query=call%20of%20duty',
+         headers: {
+           'Client-ID': '5ax5wobie94supesi1p1pmhbweb5d2',
+           'Accept': 'application/vnd.twitchtv.v5+json'
+         },
+         success: function(data) {
+           gameID = data.games[0]._id;
+           console.log(gameID);
+           console.log(data);
+           streamAjax();
+        
+         }
+    });
+    
+    
+    
+      //build url with search option-----------------------------------------------------
+    
+    
+ 
