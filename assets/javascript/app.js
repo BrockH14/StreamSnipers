8313d90c026cd43acfdc739ddd81d72703ee1da1
Brett Landes <brettl0407@gmail.com>
2020-03-21 22:47:46 -0400

    uncommented the youtube api data now that the duplicate ajax request glitch is fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,7 +55,6 @@ function streamAjax() {
          }
     });
 
- });
 
     
     
@@ -67,35 +66,35 @@ function streamAjax() {
 
 
 
-// // $("#submit").on("click", function(event) {
-// //     event.preventDefault();
-//     var userinput = $("#search").val().trim();
-//     console.log(userinput);
-//     $("#search").val("");
-// $.ajax({
-//     type: 'GET',
-//     url: 'https://www.googleapis.com/youtube/v3/search',
-//     data: {
-//         key: 'AIzaSyAqcWfiYqihUQTnDWfntvgulA7b61ZxIpg',
-//         q: userinput,
-//         part: 'snippet',
-//         maxResults: 3,
-//         type: 'video',
-//         videoEmbeddable: true,
-//     },
-//     success: function(data){
-//         console.log(data);
-//         embedVideo(data);
-//     },
-//     error: function(response){
-//         console.log("Request Failed");
-//     }
+// $("#submit").on("click", function(event) {
+//     event.preventDefault();
+    var userinput = $("#search").val().trim();
+    console.log(userinput);
+    $("#search").val("");
+$.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+        key: 'AIzaSyAqcWfiYqihUQTnDWfntvgulA7b61ZxIpg',
+        q: userinput,
+        part: 'snippet',
+        maxResults: 3,
+        type: 'video',
+        videoEmbeddable: true,
+    },
+    success: function(data){
+        console.log(data);
+        embedVideo(data);
+    },
+    error: function(response){
+        console.log("Request Failed");
+    }
     
-//   });
-//   function embedVideo(data) {
-//     $('#slide1').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
-//     $('#slide2').attr('src', 'https://www.youtube.com/embed/' + data.items[1].id.videoId)
-//     $('#slide3').attr('src', 'https://www.youtube.com/embed/' + data.items[2].id.videoId)
-// }
+  });
+  function embedVideo(data) {
+    $('#slide1').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+    $('#slide2').attr('src', 'https://www.youtube.com/embed/' + data.items[1].id.videoId)
+    $('#slide3').attr('src', 'https://www.youtube.com/embed/' + data.items[2].id.videoId)
+}
 
-// });
+});
